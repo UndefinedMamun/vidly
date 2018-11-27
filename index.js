9b2478c39371b3b4644646e755195d1d3bf18c82
undefinedmamun <bkmmamun@gmail.com>
2018-11-27 09:33:05 +0600

    more deployment ready code

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('./startup/config')();
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/validation')();
+require('./startup/production')(app);
 
 
 const PORT = process.env.PORT || 3000;
--- startup/db.js
+++ startup/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+const config = require('config');
 
 
 module.exports = function () {
-    const uri = 'mongodb://localhost/vidly';
+    const uri = config.get('db');
     mongoose.connect(uri, {
         useNewUrlParser: true,
         useCreateIndex: true
